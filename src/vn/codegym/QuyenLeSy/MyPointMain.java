a77ba0db42cba8e6e7ca33c03eed58c329290cb7
Quyen Le Sy <danquyen6596@gmail.com>
2018-10-05 09:42:06 +0700

    Add file a program MyPoint.

--- src/vn/codegym/QuyenLeSy/MyPointMain.java
+++ src/vn/codegym/QuyenLeSy/MyPointMain.java
@@ -5,7 +5,7 @@ public class MyPointMain {
         MyPoint myPoint = new MyPoint();
         MyPoint myPoint1 = new MyPoint(10, 30.5);
 
-        System.out.println("Khoảng các tọa độ 1:{" + myPoint.getX()+", "+myPoint.getY()+"} là: "+myPoint.distance());
-        System.out.println("Khoảng các tọa độ 2:{" + myPoint1.getX()+", "+myPoint1.getY()+"} là: "+myPoint1.distance());
+        System.out.println("Khoảng cách tọa độ 1:{" + myPoint.getX()+", "+myPoint.getY()+"} là: "+myPoint.distance());
+        System.out.println("Khoảng cách tọa độ 2:{" + myPoint1.getX()+", "+myPoint1.getY()+"} là: "+myPoint1.distance());
     }
 }
